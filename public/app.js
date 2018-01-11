0c7907a73b9d0656841b5d8e84bd648dedf718e1
Sean Byrne <spickles@Seans-iMac.local>
2018-01-11 17:38:30 -0500

    Get categories and Get questions routes added

--- public/app.js
+++ public/app.js
@@ -3,4 +3,29 @@ const app = angular.module('QuipProQuo', []);
 app.controller('MainController', ['$http', function($http) {
 
 	this.word = 'hi';
+	this.categories = []
+	this.questions = []
+
+// Get categories
+	http({
+		method: 'GET',
+		url: 'http://localhost:3000/categories',
+	}).then(response => {
+		console.log('Response: ', response);
+		this.categories = response.data;
+	}).catch(reject => {
+		console.log('Reject: ', reject);
+	});
+
+// Get Questions
+	http({
+		method: 'GET',
+		url: 'http://localhost:3000/categories/questions',
+	}).then(response => {
+		console.log('Response: ', response);
+		this.questions = response.data;
+	}).catch(reject => {
+		console.log('Reject: ', reject);
+	});
+
 }]);
