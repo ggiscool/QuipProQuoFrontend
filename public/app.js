4451949d3bc1603f8cd6f1066df739a4ae0bc539
Ryan Julier <rdjulier@gmail.com>
2018-01-12 09:57:47 -0800

    Updated frontend appjs and index

--- public/app.js
+++ public/app.js
@@ -5,6 +5,11 @@ app.controller('MainController', ['$http', function($http) {
 	this.word = 'hi';
 	this.categories = []
 	this.questions = []
+	this.questionID = 0;
+	this.answer = '';
+	this.display = false;
+	this.formData = {};
+	this.category = 0;
 
 // Get categories
 	$http({
@@ -27,5 +32,54 @@ app.controller('MainController', ['$http', function($http) {
 	}).catch(reject => {
 		console.log('Reject: ', reject);
 	});
+
+	this.answer = () => {
+
+		$http({
+			method: 'POST',
+			url: 'http://localhost:3000/categories/1/questions/' + this.questionID + '/answers',
+			data: this.formData
+		}).then(response => {
+			console.log('Response ', response.data);
+			this.answer = response.data;
+		}).catch(reject => {
+			console.log('Reject: ', reject);
+		});
+
+	};
+
+	this.displayForm = (question) => {
+
+		if (this.display == false) {
+
+			this.display = true;
+			this.questionID = question;
+		}
+
+		else {
+
+			this.display = false;
+		}
+	};
+
+	this.category1 = () => {
+
+		this.category = 1;
+	}
+
+	this.category2 = () => {
+
+		this.category = 2;
+	}
+
+	this.category3 = () => {
+
+		this.category = 3;
+	}
+
+	this.pickQuestion = (question) => {
+
+		this.questionID = question;
+	}
 
 }]);
