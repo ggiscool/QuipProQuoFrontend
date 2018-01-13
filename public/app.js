b956ba708af76b0305658414de3ff0fef18b090f
Ryan Julier <rdjulier@gmail.com>
2018-01-12 16:34:50 -0800

    Updates to frontend

--- public/app.js
+++ public/app.js
@@ -6,11 +6,12 @@ app.controller('MainController', ['$http', function($http) {
 	this.categories = []
 	this.questions = []
 	this.questionID = 0;
-	this.answer = '';
+	this.findanswer = [];
 	this.display = false;
 	this.formData = {};
 	this.category = 0;
 	this.viewAnswersModal = false
+	this.currentquestion = false;
 	// this.showQuestion = 0;
 
 // Get categories
@@ -64,32 +65,62 @@ app.controller('MainController', ['$http', function($http) {
 		}
 	};
 
-	// this.category1 = () => {
-  //
-	// 	this.category = 1;
-	// }
-  //
-	// this.category2 = () => {
-  //
-	// 	this.category = 2;
-	// }
-  //
-	// this.category3 = () => {
-  //
-	// 	this.category = 3;
-	// }
+	this.category1 = () => {
+  
+		this.category = 1;
+		this.viewAnswersModal = false;
+		this.questionID = 0;
+	}
+  
+	this.category2 = () => {
+  
+		this.category = 2;
+		this.viewAnswersModal = false;
+		this.questionID = 0;
+	}
+  
+	this.category3 = () => {
+  
+		this.category = 3;
+		this.viewAnswersModal = false;
+		this.questionID = 0;
+	}
 
 	this.pickQuestion = (question) => {
 
 		this.questionID = question;
+
+		if (this.currentquestion == true) {
+
+			this.viewAnswersModal = false;
+		}
+
 	}
 
-this.viewAnswers = () => {
+this.viewAnswers = (question) => {
+
+	$http({
+		method: 'GET',
+		url: 'http://localhost:3000/categories/1/questions/' + this.questionID + '/answers'
+	}).then(response => {
+		console.log('Response: ', response);
+		this.findanswer = response.data;
+	}).catch(reject => {
+			console.log('Reject: ', reject);
+	});
+
 	if (this.viewAnswersModal == false) {
 		this.viewAnswersModal = true
-	}else{
-		this.viewAnswersModal =false
 	}
+
+	if (this.currentquestion == false) {
+
+		this.currentquestion = true;
+		console.log('Viewing answer');
+	}
+
+
+	this.questionID = question;
 }
 
 
